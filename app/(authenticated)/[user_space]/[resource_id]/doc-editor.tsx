75785702b356e51f3fb44dae24389764cf403baf
jerry_lllman <jerry_lllman@163.com>
2025-04-11 22:05:55 +0800

    refactor(editor): move editor logic to custom hook
    Extract editor initialization and shortcut handling logic into a reusable custom hook `usePandaEditor` to improve code maintainability and reduce duplication in the `DocEditor` component.

--- app/(authenticated)/[user_space]/[resource_id]/doc-editor.tsx
+++ app/(authenticated)/[user_space]/[resource_id]/doc-editor.tsx
@@ -1,59 +1,13 @@
-import './styles.css'
+import { EditorContent } from '@tiptap/react'
 
-import { EditorContent, useEditor } from '@tiptap/react'
-import { StarterKit } from '@tiptap/starter-kit'
-import { TextStyle } from '@tiptap/extension-text-style'
-import { Underline } from '@tiptap/extension-underline'
-import Highlight from '@tiptap/extension-highlight'
-
-import { Color, Link, ResetMarksOnEnter, Selection } from './extensions'
 import { TopToolbar } from './components'
-import { useEffect } from 'react'
+import { usePandaEditor } from './hooks/use-panda-editor'
+
+import './styles.css'
 
 export function DocEditor() {
 
-  const editor = useEditor({
-    editable: true,
-    extensions: [
-      StarterKit.configure({
-        horizontalRule: false,
-        codeBlock: false,
-        paragraph: { HTMLAttributes: { class: 'text-node' } },
-        heading: { HTMLAttributes: { class: 'heading-node' } },
-        blockquote: { HTMLAttributes: { class: 'block-node' } },
-        bulletList: { HTMLAttributes: { class: 'list-node' } },
-        orderedList: { HTMLAttributes: { class: 'list-node' } },
-        code: { HTMLAttributes: { class: 'inline inline-code', spellcheck: 'false' } },
-        dropcursor: { width: 2, class: 'ProseMirror-dropcursor border' }
-      }),
-      Highlight.configure({ multicolor: true }),
-      Selection,
-      Underline,
-      TextStyle,
-      Color,
-      Link,
-      ResetMarksOnEnter
-    ],
-    content: `
-      <p>
-        Wow, this editor has support for links to the whole <a href="https://en.wikipedia.org/wiki/World_Wide_Web">world wide web</a>. We tested a lot of URLs and I think you can add *every URL* you want. Isn’t that cool? Let’s try <a href="https://statamic.com/">another one!</a> Yep, seems to work.
-      </p>
-      <p>
-        By default every link will get a <code>rel="noopener noreferrer nofollow"</code> attribute. It’s configurable though.
-      </p>
-    `,
-  })
-
-  useEffect(() => {
-    // Stop editor shortcuts from bubbling up to the document
-    const stopShortcuts = (e: KeyboardEvent) => {
-      if ((e.metaKey || e.ctrlKey) && editor?.isFocused) {
-        e.stopPropagation()
-      }
-    }
-    document.addEventListener('keydown', stopShortcuts)
-    return () => document.removeEventListener('keydown', stopShortcuts)
-  }, [editor])
+  const { editor } = usePandaEditor()
 
   if (!editor) {
     return null
