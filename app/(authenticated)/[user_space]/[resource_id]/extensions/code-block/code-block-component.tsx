797769ff85da0ebe2e84981ec9f8bd7634fd3f3a
jerry_lllman <jerry_lllman@163.com>
2025-05-06 22:09:33 +0800

    feat(code-block): add language search functionality and improve styling
    Introduce a search input to filter available languages in the code block component. Enhance the styling of the code block to improve readability and user experience.

--- app/(authenticated)/[user_space]/[resource_id]/extensions/code-block/code-block-component.tsx
+++ app/(authenticated)/[user_space]/[resource_id]/extensions/code-block/code-block-component.tsx
@@ -3,29 +3,246 @@ import { NodeViewContent, NodeViewProps, NodeViewWrapper } from '@tiptap/react'
 import { Select, SelectTrigger, SelectContent, SelectItem } from '@/components/ui/select'
 import React from 'react'
 import { SelectGroup } from '@radix-ui/react-select'
+import { Input } from '@/components/ui/input'
+
+const languages = [
+  {
+    label: 'Plain Text',
+    value: 'plaintext',
+  },
+  {
+    label: 'Bash',
+    value: 'bash',
+  },
+  {
+    label: 'Basic',
+    value: 'basic',
+  },
+  {
+    label: 'C',
+    value: 'c',
+  },
+  {
+    label: 'C#',
+    value: 'csharp'
+  },
+  {
+    label: 'C++',
+    value: 'cpp',
+  },
+  {
+    label: 'CSS',
+    value: 'css',
+  },
+  {
+    label: 'Dart',
+    value: 'dart',
+  },
+  {
+    label: 'Diff',
+    value: 'diff',
+  },
+  {
+    label: 'Dockerfile',
+    value: 'dockerfile',
+  },
+  {
+    label: 'Git',
+    value: 'git',
+  },
+  {
+    label: 'Go',
+    value: 'go',
+  },
+  {
+    label: 'GraphQL',
+    value: 'graphql',
+  },
+  {
+    label: 'Haskell',
+    value: 'haskell',
+  },
+  {
+    label: 'HTML',
+    value: 'html',
+  },
+  {
+    label: 'HTTP',
+    value: 'http',
+  },
+  {
+    label: 'Java',
+    value: 'java',
+  },
+  {
+    label: 'JavaScript',
+    value: 'javascript',
+  },
+  {
+    label: 'JSON',
+    value: 'json',
+  },
+  {
+    label: 'JSX',
+    value: 'jsx',
+  },
+  {
+    label: 'Julia',
+    value: 'julia',
+  },
+  {
+    label: 'Kotlin',
+    value: 'kotlin',
+  },
+  {
+    label: 'Less',
+    value: 'less',
+  },
+  {
+    label: 'Markdown',
+    value: 'markdown',
+  },
+  {
+    label: 'Nginx',
+    value: 'nginx',
+  },
+  {
+    label: 'Objective-C',
+    value: 'objectivec',
+  },
+  {
+    label: 'PHP',
+    value: 'php',
+  },
+  {
+    label: 'PL/SQL',
+    value: 'plsql',
+  },
+  {
+    label: 'PowerShell',
+    value: 'powershell',
+  },
+  {
+    label: 'Protobuf',
+    value: 'protobuf',
+  },
+  {
+    label: 'Python',
+    value: 'python',
+  },
+  {
+    label: 'R',
+    value: 'r',
+  },
+  {
+    label: 'Ruby',
+    value: 'ruby',
+  },
+  {
+    label: 'Rust',
+    value: 'rust',
+  },
+  {
+    label: 'Sass',
+    value: 'sass',
+  },
+  {
+    label: 'Scala',
+    value: 'scala',
+  },
+  {
+    label: 'Scheme',
+    value: 'scheme',
+  },
+  {
+    label: 'Shell',
+    value: 'shell',
+  },
+  {
+    label: 'Solidity',
+    value: 'solidity',
+  },
+  {
+    label: 'SQL',
+    value: 'sql',
+  },
+  {
+    label: 'Swift',
+    value: 'swift',
+  },
+  {
+    label: 'TOML',
+    value: 'toml',
+  },
+  {
+    label: 'TSX',
+    value: 'tsx',
+  },
+  {
+    label: 'TypeScript',
+    value: 'typescript',
+  },
+  {
+    label: 'Vue',
+    value: 'vue',
+  },
+  {
+    label: 'XML',
+    value: 'xml',
+  },
+  {
+    label: 'YAML',
+    value: 'yaml',
+  },
+]
+
 
 export const CodeBlockComponent = (props: NodeViewProps) => {
   const { node: { attrs: { language: defaultLanguage } }, updateAttributes, extension } = props
-  return (
 
+  const [searchLanguage, setSearchLanguage] = React.useState<string>('')
+
+  // 添加语言列表缓存
+  const allLanguages = React.useMemo(() => {
+    const listLanguages: string[] = extension.options.lowlight.listLanguages()
+    console.log('%c👉  listLanguages: ', 'background:#41b883;padding:1px; border-radius: 0 3px 3px 0;color: #fff', listLanguages) // 👈
+    return languages.filter(item => listLanguages.findIndex(lang => lang === item.value))
+  }, [extension.options.lowlight])
+
+  const filteredLanguages = React.useMemo(() => {
+    return allLanguages.filter(item => item.label.toLowerCase().includes(searchLanguage.toLowerCase()))
+  }, [allLanguages, searchLanguage])
+
+  return (
     <NodeViewWrapper className="code-block">
-      <Select onValueChange={language => updateAttributes({ language })}>
-        <SelectTrigger>
-          {defaultLanguage}
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            {extension.options.lowlight.listLanguages().map((lang: string) => (
-              <SelectItem key={lang} value={lang}>
-                {lang}
-              </SelectItem>
-            ))}
-          </SelectGroup>
-        </SelectContent>
-      </Select>
-      <pre>
-        <NodeViewContent as="code" />
-      </pre>
+      <div>
+        <Select onValueChange={language => updateAttributes({ language })}>
+          <SelectTrigger>
+            {defaultLanguage}
+          </SelectTrigger>
+          <SelectContent>
+            <Input
+              placeholder="Search for a language..."
+              onChange={e => {
+                const value = e.target.value
+                setSearchLanguage(value)
+              }}
+            />
+            <SelectGroup>
+              {filteredLanguages.map((language) => (
+                <SelectItem key={language.value} value={language.value}>
+                  {language.label}
+                </SelectItem>
+              ))}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </div>
+      <div onKeyDown={e => { e.stopPropagation(); e.preventDefault() }}>
+        <pre>
+          <NodeViewContent as="code" />
+        </pre>
+      </div>
     </NodeViewWrapper>
   )
 }
