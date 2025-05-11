2dfe557a805d852744ff0c3a25fd2f9b9a3ec027
jerry_lllman <jerry_lllman@163.com>
2025-05-11 21:48:51 +0800

    feat: add lodash-es for utility functions and update CodeBlockComponent for improved language filtering
    This commit introduces the lodash-es library to the project, enhancing utility function capabilities. Additionally, the CodeBlockComponent is updated to utilize lodash-es for trimming search input, improving the language filtering logic and overall user experience.

--- src/pages/extensions/code-block/code-block-component.tsx
+++ src/pages/extensions/code-block/code-block-component.tsx
@@ -1,10 +1,11 @@
 import { NodeViewContent, NodeViewWrapper } from '@tiptap/react'
 import type { NodeViewProps as NodeViewPropsType } from '@tiptap/react'
-import { App, Divider, Dropdown, Input, Space, theme } from 'antd'
+import { App, Divider, Input, Select } from 'antd'
 import React from 'react'
 import copy from 'copy-to-clipboard'
-import { ChevronDown, Copy } from 'lucide-react'
+import { Copy } from 'lucide-react'
 import { TooltipButton } from '@/pages/components'
+import { trim } from 'lodash-es'
 
 const languages = [
   {
@@ -208,18 +209,16 @@ export const CodeBlockComponent = (props: NodeViewPropsType) => {
   // get enabled languages
   const allLanguages = React.useMemo(() => {
     const listLanguages: string[] = extension.options.lowlight.listLanguages()
-    return languages.filter(item => listLanguages.findIndex(lang => lang === item.value)).map(item => ({
-      key: item.value,
-      label: item.label,
-      onClick: () => {
-        updateAttributes({ language: item.value })
-        // editor.commands.focus()
-      }
-    }))
-  }, [extension.options.lowlight, updateAttributes])
+    return languages.filter(item => listLanguages.findIndex(lang => lang === item.value))
+  }, [extension.options.lowlight])
 
   const filteredLanguages = React.useMemo(() => {
-    return allLanguages.filter(item => item.label.toLowerCase().includes(searchLanguage.toLowerCase()))
+    const search = trim(searchLanguage)
+    if (!search) {
+      return allLanguages
+    }
+
+    return allLanguages.filter(item => item.label.toLowerCase().includes(search.toLowerCase()))
   }, [allLanguages, searchLanguage])
 
   const copyCode = () => {
@@ -228,42 +227,25 @@ export const CodeBlockComponent = (props: NodeViewPropsType) => {
     message.success('Copied code to clipboard')
   }
 
-
-  const { token } = theme.useToken()
-
-  const contentStyle: React.CSSProperties = {
-    backgroundColor: token.colorBgElevated,
-    borderRadius: token.borderRadiusLG,
-    boxShadow: token.boxShadowSecondary,
-  };
-
   return (
     <NodeViewWrapper className="code-block rounded-md border border-gray-200 p-2 my-2">
       <div className='flex justify-between items-center border-b border-gray-200 pb-2' contentEditable='false'>
-        <Dropdown
-          trigger={['click']}
-          menu={{ items: filteredLanguages }}
-
+        <Select
+          options={filteredLanguages}
+          value={defaultLanguage}
+          onChange={(value) => updateAttributes({ language: value })}
+          popupMatchSelectWidth={300}
+          variant='borderless'
           popupRender={(menu) => (
-            <div style={contentStyle}>
+            <div>
               <div className='p-2'>
-                <Input placeholder='Search Language' onChange={(e) => setSearchLanguage(e.target.value)} />
+                <Input placeholder='Search for a language' value={searchLanguage} onChange={(e) => setSearchLanguage(e.target.value)} />
               </div>
               <Divider style={{ margin: 0 }} />
-              {React.cloneElement(
-                menu as React.ReactElement<{
-                  style: React.CSSProperties;
-                }>,
-                { style: { boxShadow: 'none', maxHeight: 520, overflow: 'auto' } },
-              )}
+              {menu}
             </div>
           )}
-        >
-          <Space size="small" className='cursor-pointer text-gray-500 hover:bg-gray-200 duration-300 rounded-md px-2'>
-            {defaultLanguage}
-            <ChevronDown size={14} />
-          </Space>
-        </Dropdown>
+        />
         <div className='px-2 '>
           <TooltipButton
             className='!p-1'
