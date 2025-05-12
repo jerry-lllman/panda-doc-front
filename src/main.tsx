f61cb1fb8fb4fbb3c82d588c5027bc5348e22922
jerry_lllman <jerry_lllman@163.com>
2025-05-12 11:02:04 +0800

    feat(dependencies): add @ant-design/v5-patch-for-react-19 to support React 19 compatibility

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { App } from 'antd'
+import '@ant-design/v5-patch-for-react-19';
 import AppRouter from './router'
 import './index.css'
 import './assets/styles/theme.css'
-import { App } from 'antd'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App>
